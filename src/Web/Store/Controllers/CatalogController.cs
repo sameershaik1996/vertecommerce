f8e54d94ef5590d533f4387693bc116f81ecca66
Sameer <ssameer@projectverte.com>
2019-02-11 11:08:26 +0530

    product service and client, base token service

--- src/Web/Store/Controllers/CatalogController.cs
+++ src/Web/Store/Controllers/CatalogController.cs
@@ -26,24 +26,24 @@ namespace Store.Controllers
 			int pageId = (int)page + 1;
 			var catalog =  _catalogService.GetProducts(pageId, itemsPage, BrandFilterApplied, TypesFilterApplied);
 
+
 			
-
-			var vm = new CatalogIndexViewModel()
-			{
-				CatalogItems = catalog.Data,
-				Brands =  _catalogService.GetBrands(),
-				Types =  _catalogService.GetCategories(),
-				BrandFilterApplied = BrandFilterApplied ?? 0,
-				TypesFilterApplied = TypesFilterApplied ?? 0,
-				PaginationInfo = new PaginationInfo()
+				var vm = new CatalogIndexViewModel()
 				{
-					ActualPage = page ?? 0,
-					ItemsPerPage = itemsPage, //catalog.Data.Count,
-					TotalItems = catalog.Count,
-					TotalPages = (int)Math.Ceiling(((decimal)catalog.Count / itemsPage))
-				}
-			};
-
+					CatalogItems = catalog.Data,
+					Brands = _catalogService.GetBrands(),
+					Types = _catalogService.GetCategories(),
+					BrandFilterApplied = BrandFilterApplied ?? 0,
+					TypesFilterApplied = TypesFilterApplied ?? 0,
+					PaginationInfo = new PaginationInfo()
+					{
+						ActualPage = page ?? 0,
+						ItemsPerPage = itemsPage, //catalog.Data.Count,
+						TotalItems = catalog.Count,
+						TotalPages = (int)Math.Ceiling(((decimal)catalog.Count / itemsPage))
+					}
+				};
+			
 			vm.PaginationInfo.Next = (vm.PaginationInfo.ActualPage == vm.PaginationInfo.TotalPages - 1) ? "is-disabled" : "";
 			vm.PaginationInfo.Previous = (vm.PaginationInfo.ActualPage == 0) ? "is-disabled" : "";
 
